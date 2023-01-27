fbafb693e36fe16c42ccdddaf5416393c3c6451b
Wafeeq Izath <120131070+ezath97@users.noreply.github.com>
2023-01-27 23:10:03 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,8 @@
+
+# Create a JSON file (employee.json) containing employee information of minimum 5 employees. 
+# Each employee information consists of Name, DOB, Height, City, State. 
+# Write a python program that reads this information from the JSON file and saves the information into a list of objects of Employee class. 
+# Finally print the list of the Employee objects.
+# Create a dictionary of any 7 Indian states and their capitals. Write this into a JSON file.
+
+# Solution
